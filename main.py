d9ffb54be7bcb2f673c0051ae126e052d6e9d3a6
tibbletoes <158589692+tibbletoes@users.noreply.github.com>
2024-06-19 18:44:44 +0000

    still buggy but abt to make a big-ish change soooo

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ class Farmer:
   def ShowCurrency(self):
     print(f'Balance: {self.currency}')
 
-Player = Farmer(500)
 
 class Inventory:
   def __init__(self):
@@ -30,41 +29,44 @@ class Inventory:
   def ShowInventory(self):
     print(self.inventory)
 
+Player = Farmer(500)
+PlayerInv = Inventory()
 
 def Shop(Item):
   print("Welcome! What can I do for ya'?")
-  choice = input("[1] Buy \n [2] Sell \n [3] Leave")
+  choice = input("\n [1] Buy \n [2] Sell \n [3] Leave")
   if choice == "1":
-    print(f'[1] {WheatSeeds}')
-    print(f'[2] {BlueberrySeeds}')
-    print(f'[3] {HyacinthSeeds}')
-    print(f'[4] {RaspberrySeeds}')
-    print(f'[5] {StrawberrySeeds}')
-    print(f'[6] {WatermelonSeeds}')
-    print(f'[7] {PearSapling}')
-    print(f'[8] {CherrySapling}')
-    print(f'[9] {LemonSapling}')
+    print(f'[1] {Item.WheatSeeds}')
+    print(f'[2] {Item.BlueberrySeeds}')
+    print(f'[3] {Item.HyacinthSeeds}')
+    print(f'[4] {Item.RaspberrySeeds}')
+    print(f'[5] {Item.StrawberrySeeds}')
+    print(f'[6] {Item.WatermelonSeeds}')
+    print(f'[7] {Item.PearSapling}')
+    print(f'[8] {Item.CherrySapling}')
+    print(f'[9] {Item.LemonSapling}')
     print("[10] Leave")
     choice = input("What would you like to buy?")
-    if choice == "1":
-      amtchoice = input("How many would you like to buy?")
-      if amtchoice * WheatSeeds.price <= Player.currency:
-        Player.SubCurrency(amtchoice * WheatSeeds.price)
-        Inventory.AddItem(amtchoice * WheatSeeds)
-      else:
-        print("You don't have enough money!")
+    if choice == "1" and Player.currency >= Item.WheatSeeds.price:
+        Player.SubCurrency(Item.WheatSeeds.price)
+        PlayerInv.AddItem(Item.WheatSeeds)
+        
+        
+        
       
 
 def MainMenu():
-  print("[1] Farm \n [2] Inventory \n [3] Shop \n [4] Quit")           
+  print("\n [1] Farm \n [2] Inventory \n [3] Shop \n [4] Quit")           
   choice = input("What would you like to do? ")
   if choice == "1":
     pass
   elif choice == "2":
     pass
   elif choice == "3":
-    pass
+    ItmInst = items.Item
+    Shop(ItmInst)
   elif choice == "4":
     print("Whatever, Hater.")
     sys.exit()
 
+MainMenu()
